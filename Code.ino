e8940f9c158d2548ec36c0e2248c5f104afa9418
yaseemarusiru <112842373+yaseemarusiru@users.noreply.github.com>
2023-05-28 07:42:15 +0530

    Revision 2 - working up to weight entering part

--- Code.ino
+++ Code.ino
@@ -1,20 +1,31 @@
+//HX711 Amplifier
 #include "HX711.h"
+#include <SoftwareSerial.h>
+
+//OLED Dislpay
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define DT A2 
-#define SCK A3 
-#include <SoftwareSerial.h>
+
+////----HX711 Amplifier---------
+#define DT A2
+#define SCK A3
+
+//----------OLED Dislpay-------------
 #define SCREEN_WIDTH 128 //In pixels
 #define SCREEN_HEIGHT 64 //In pixels
+//Hardware SPI
 #define OLED_DC     9
-#define OLED_CS     12
+#define OLED_CS     8
 #define OLED_RESET  10
+// Number of snowflakes in the animation
 #define NUMFLAKES     30 
+//Logo dimensions
 #define LOGO_HEIGHT   16
 #define LOGO_WIDTH    16
-#define XPOS   0 
+//For the drawSnowflakes function
+#define XPOS   0 // Indexes into the 'icons' array in function below
 #define YPOS   1
 #define DELTAY 2
 
@@ -25,22 +36,21 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT,
 
 SoftwareSerial mySerial(2,3);
 
+String data ;
+char c;
 float scalingFactor = 537;
-String stringWeight;
-int weight = atoi(stringWeight.c_str());
-float recomendedAmountPerHour = weight*0.033*1000/16;
+float weight = 50; //dummy weight - should take from wifi module
+float recomendedAmount = weight*0.033;
+float recomendedAmountPerHour = recomendedAmount/16;
 int percentageOfWater = 0;
 float w1,w2;
 float consumptionTotal = 0;
 float consumptionPerHour = 0;
-int prevHour=0, currentHour=0;
-int prevMinute=0, currentMinute=0;
-bool flag1 = true;
+int prevHour=1, currentHour=2;
+int prevMinute=3, currentMinute=4;
+bool flag1 = true, flag2 = true, flag3 = true;
 int x=14+ 46*(1-0.01*percentageOfWater);
 int m;
-String data ;
-String newcp =data.substring(0,5);
-char c;
 
 char teamName[]={'S','P','A','R','T','A','N','S'};
 String stratString[]={" Smart"," Water","Traking"," Device"};
@@ -62,18 +72,16 @@ static const unsigned char PROGMEM logo_bmp[] =
   B01110000, B01110000,
   B00000000, B00110000 };
 
-
-
 void setup() {
+  mySerial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC)) {
+    for(;;); // If display is not ready, don't proceed, loop forever
+  }
 
-  //Initializing for the WiFi module
-  mySerial.begin(115200);
-  //while(!display.begin(SSD1306_SWITCHCAPVCC)) {
-    //for(;;); // If display is not ready, don't proceed, loop forever
-  //}
   //the library initializes this with an Adafruit splash screen.
   display.display();
-  delay(200); // Pause for 1 seconds
+  delay(1000); // Pause for 1 seconds
+
   //display team name
   displaySpartans();
   display.invertDisplay(false);
@@ -83,89 +91,74 @@ void setup() {
   displayStartString();
   display.clearDisplay();
   delay(200);
-
+  while (flag2){
+    while(mySerial.available()>0){
+      delay(10);
+      c = mySerial.read();
+      data += c; 
+    } 
+    if (data.length()>0){
+      weight = atoi(data.c_str());
+      if (weight>0) { 
+        flag2 = false;
+        display.println(weight);
+        display.display();
+      }
+    data = "";
+    }
+  }
+  
   loadCell.begin(DT,SCK);  //initialize the pin modes and sets the gain to 128
   loadCell.tare();  //0 will be the weight measured at this time
   loadCell.set_scale(scalingFactor);  //used to divide the reading to measure weight in grams
   w1 = loadCell.get_units(7); //initial weight
-  while (!started());
-  while (!weightEntered());
 }
 
 void loop() {
-  mySerialRead();
-  while (readTime());
-  displayTime();
-  mySerialRead();
-  showConsumptionOnTelegram();
+  delay(1000);
+  while(mySerial.available()>0){
+      delay(10);
+      c = mySerial.read();
+      data += c; 
+    } 
+  
+  if (data.length()>0){
+    if (data=="CU"){
+      mySerial.println(consumptionTotal);
+      data = "";
+    }
+    else if (data=="CL"){
+      float left = recomendedAmount - consumptionTotal;
+      mySerial.println(left);
+      data = "";
+    }
+    data = "";
+  }
+  
   w2 = loadCell.get_units(7);
+ 
+  displayConsumption(&percentageOfWater);
   //updating a consumption
-  if (w1-w2>10) {
+  if (w1-w2>1) {
     consumptionPerHour += (w1-w2) ;
     consumptionTotal += (w1-w2);
-    percentageOfWater = consumptionTotal/weight*0.033*1000*100;
+    percentageOfWater = consumptionTotal/recomendedAmount*100;
   }
- displayConsumption();
+
   //checking if 1 hour has passed since the user started the device
-  if ((currentHour-prevHour == 1) && (currentMinute-prevMinute == 0)){
+   if ((currentHour-prevHour == 1) && (currentMinute-prevMinute == 0)){
     if (consumptionPerHour - recomendedAmountPerHour > 20) {
       displayConsumedRecAmount();
-      mySerial.print("AA");
+      //telegram
     }
     else {
       displayNotConsumedRecAmount();
-      mySerial.print("BB");
+      //telegram
     }
     consumptionPerHour = 0;
   }
+
   w1 = w2;
-}
-
-
-void mySerialRead(){
-  while(mySerial.available()>0){
-    delay(10);
-    c = mySerial.read();
-    data += c;
-  } 
-}
-
-bool started(){
-  mySerialRead();
-  if (data.length()>0) {
-    if (newcp == "START") {
-      data = "";
-      return true;
-    }
-    data = "";
-  }
-  return false;
-}
-
-bool weightEntered(){
-  mySerialRead();
-  if (data.length()>0) {
-    if (isDigit(data[0]) && isDigit(data[1]) && data.length()<3) {
-      stringWeight = data;
-      data = "";
-      return true;
-    } 
-  return false;
-  }
-}
-
-void showConsumptionOnTelegram(){
-  if (data.length()>0) {
-     if (data == "CU") {  //current consumption
-      mySerial.print(percentageOfWater);
-      data = "";
-    } 
-    else if (data == "CL") {  //left to consume
-      mySerial.print(100 - percentageOfWater);
-      data = "";
-    }
-    data = "";
-  }
 }
 
 void displaySpartans(){
@@ -194,32 +187,31 @@ void displayStartString(){
   }  
 }
 
-void displayConsumption(){
+void displayConsumption(int *percentage_Water){
   if (flag1) {
     m=0;
     flag1 = false; 
   }
-
   if(m!=x){
     drawSnowflakes(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); 
     m=x;
   }
 
-  x=14+ 46*(1-0.01*(percentageOfWater));//set coordinate
-  if(percentageOfWater<101){
+  x=14+ 46*(1-0.01*(*percentage_Water));//set coordinate
+  if(*percentage_Water<101){
     display.clearDisplay();
     display.drawRoundRect(0,14,40,49,5,WHITE);
     display.fillRoundRect(4,x,32,(60-x),5,WHITE);
-    display.drawRect(16,11,8,5,WHITE);
-    display.fillRect(16,11,8,5,WHITE);
+    display.drawRect(16,11,8,3,WHITE);
+    display.fillRect(16,11,8,3,WHITE);
     display.setCursor(50,30);
     display.setTextSize(4);
-    display.print(percentageOfWater);//display percentage
+    display.print(*percentage_Water);//display percentage
     display.print("%");
     displayTime();
     display.display();
     delay(2000);
-    //display.clearDisplay();
+    display.clearDisplay();
   }
 
   else {
@@ -228,31 +220,17 @@ void displayConsumption(){
     display.setCursor(10,0);
     display.println();
     display.println(" You Have");
-    display.println("  Drunk ");
+    display.println("  Drink ");
     display.println(" Enough");
     displayTime();
     display.display();
     delay(1500);
   }
-
-}
-
-bool readTime(){
-  if (data.substring(0,5)=="ASDRT"){
-    prevHour = currentHour;
-    prevMinute = currentMinute;
-    String l=data.substring(5,9);
-    String p = data.substring(5,9);
-    currentHour = (int) atof(l.substring(0,2).c_str());
-    currentMinute = (int) atof(p.substring(2,4).c_str());
-    data = "";
-    return true;
-  }
-  return false;
+  
 }
 
 void displayTime(){
-  display.setCursor(44,0);
+  display.setCursor(50,0);
   display.setTextSize(2);
 
   if(currentHour<13){
@@ -262,10 +240,7 @@ void displayTime(){
   else {
     display.print(currentHour+String(".")+currentMinute+String("pm"));  //display time pm
     display.display();
-      display.print("ok");
   }
-
-  delay(300);
 }
 
 void testdrawbitmap() {
@@ -294,7 +269,7 @@ void drawSnowflakes(const uint8_t *bitmap, uint8_t w, uint8_t h) {
     }
 
     display.display(); // Show the display buffer on the screen
-    delay(100);        // Pause for 1/10 second
+    delay(200);        // Pause for 1/10 second
 
     // Then update coordinates of each flake...
     for(f=0; f< NUMFLAKES; f++) {
@@ -317,7 +292,7 @@ void displayConsumedRecAmount(){
   display.drawCircle(64, 32, 27, WHITE);
   display.drawCircle(64, 32, 28, WHITE);
   display.display();
-  delay(1500);
+  delay(1000);
   display.clearDisplay();
   display.setTextSize(2);
   display.setCursor(10,0);
@@ -326,7 +301,7 @@ void displayConsumedRecAmount(){
   display.println(" Drunk Enough");
   display.println("for Hour!");
   display.display();
-  delay(1500);
+  delay(1000);
 }
 
 void displayNotConsumedRecAmount(){ 
@@ -336,7 +311,7 @@ void displayNotConsumedRecAmount(){
   display.drawCircle(64, 32, 27, WHITE);
   display.drawCircle(64, 32, 28, WHITE);
   display.display();
-  delay(1500);
+  delay(1000);
   display.clearDisplay();
   display.invertDisplay(true);
   display.setTextSize(2);
@@ -347,5 +322,5 @@ void displayNotConsumedRecAmount(){
   display.println(" Enough");
   display.display();
   display.invertDisplay(false);
-  delay(10000);
+  delay(1000);
 }
