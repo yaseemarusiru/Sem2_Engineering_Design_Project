ec6861faf98f8341f899dcd66207f737ece30a92
yasodalasiru <112842317+yasodalasiru@users.noreply.github.com>
2023-07-14 11:47:49 +0530

    Final working code without 100%

--- ESPcode.ino
+++ ESPcode.ino
@@ -3,13 +3,11 @@
 #include <UniversalTelegramBot.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
-//for time
-
 
 // Wifi network station credentials
 #define WIFI_SSID "Redmi 8A Dual 1"
 #define WIFI_PASSWORD "adee15057e3a"
-// Telegram BOT Token (Get from Botfather)
+// Telegram BOT Token (from Botfather)
 #define BOT_TOKEN "5740874696:AAGWgx4S0qCcXfDkugcrJXca9TNVhOHpB-0"
 
 const unsigned long BOT_MTBS = 200; // mean time between scan messages 1000 before 200 now
@@ -17,6 +15,14 @@ String chat_id = "1467323353";
 String data;
 char c;
 unsigned long bot_lasttime;          // last time messages' scan has been done
+
+String currhour1; 
+String minutes1; 
+String secs1;
+
+int currhour;
+int minutes;
+int secs;
 
 const long off = 19800; // next 3 lines are for time
 WiFiUDP ntpUDP;
@@ -26,130 +32,120 @@ X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 WiFiClientSecure secured_client;
 UniversalTelegramBot bot(BOT_TOKEN, secured_client);
 
-//String test_photo_url = "https://www.arduino.cc/en/uploads/Trademark/ArduinoCommunityLogo.png";
-
 void handleNewMessages(int numNewMessages) {
-  //Serial.print("handleNewMessages ");
-  //Serial.println(numNewMessages);
 
   for (int i=0; i<numNewMessages; i++) {
-    //String chat_id = bot.messages[i].chat_id;
+
     String text = bot.messages[i].text;
 
     String from_name = bot.messages[i].from_name;
+
     if (from_name == "") from_name = "Guest";
 
-    if (text == "/start") {
-      bot.sendMessage(chat_id,"Starting your device for the day");
-      //Serial.print("START");
-    }
-    // if (text == "/stop") {
-    //   bot.sendMessage(chat_id,"Stopping your device for the day");
-    //   Serial.print("STOP");
-    // }
-    // if (text == "/cu") {
-    //   Serial.print("CU");
-    //   delay(500);
-    //   while(Serial.available()>0){
-    //     delay(10);
-    //     c = Serial.read();
-    //     data += c;
-    //   } 
-    //   bot.sendMessage(chat_id,"Current consumption ",data); 
-    //   data="";
-    // }
-    // if (text == "/cl") {
-    //   Serial.print("CL");
-    //   delay(500);
-    //   while(Serial.available()>0){
-    //     delay(10);
-    //     c = Serial.read();
-    //     data += c;
-    //   } 
-    //   bot.sendMessage(chat_id,"Left to consume ",data); 
-    //   data="";
-    // }
     if (isDigit(text[0]) && isDigit(text[1]) ) {
       bot.sendMessage(chat_id,"You just entered your weight.");
       Serial.print("W"+text);
     }
+
   }
+
 }
 void arduinomsg(){
   while(Serial.available()>0){
         delay(10);
         c = Serial.read();
         data += c;
-      } 
-      if (data=="100"){
-      bot.sendMessage(chat_id,"You have drunk enough water for the past hour. Keep going :)"); 
-      data="";
-      }
-      if (data.length()>0){
-        bot.sendMessage(chat_id,"You have drunk"+data+" for the past hour"); 
-      data="";
-      }
+  } 
+  // if (data=="100"){
+  // bot.sendMessage(chat_id,"You have drunk enough water for the day. Yippy :)"); 
+  // data="";
+  // }
+  if (data=="0.00"){
+    data = "";
+  }
+  if (data.length()>0){
+    bot.sendMessage(chat_id,"You have drunk "+data+"ml for the day. Keep going :)"); 
+    data="";
+  }
+  data = "";
 }
 void setup()
 {
   Serial.begin(9600);
-  //Serial.println();
 
   // attempt to connect to Wifi network:
   configTime(19800, 0, "pool.ntp.org");      // get UTC time via NTP
   secured_client.setTrustAnchors(&cert); // Add root certificate for api.telegram.org
-  // Serial.print("Connecting to Wifi SSID ");
-  // Serial.print(WIFI_SSID);
+ 
   WiFi.disconnect();
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
   while (WiFi.status() != WL_CONNECTED)
   {
-    //Serial.print(".");
     delay(500);//
   }
-  //WiFi.enableInsecureWEP(true);
-  //Serial.print("\nWiFi connected. IP address: ");
-  //Serial.println(WiFi.localIP());
 
-  // Check NTP/Time, usually it is instantaneous and you can delete the code below.
-  //Serial.print("Retrieving time: ");
   time_t now = time(nullptr);
+
   while (now < 24 * 3600)
   {
-    //Serial.print(".");
     delay(100);
     now = time(nullptr);
   }
-  //Serial.println(now);
+
   String wel = "Welcome to Aqua Tracking Bot. You'll get hourly updates of your consumption through me :) \n";
   wel += "\n";
   wel += "Enter your weight in kgs to get the correct recommendations. You can always change the value just by sending it to me";
+  wel += "\n";
+  wel+= "Countdown starts now";
   bot.sendMessage(chat_id, wel);
-  String welo = "Countdown starts now";
-  bot.sendMessage(chat_id, welo);
+  // String welo = "Countdown starts now";
+  // bot.sendMessage(chat_id, welo);
   timeClient.begin();
   timeClient.setTimeOffset(19800);
-
-
+  timeClient.update();
 }
 
 void loop()
 {
-  timeClient.update();
-  int currhour = timeClient.getHours();
-  int minutes = timeClient.getMinutes();
-  int secs = timeClient.getSeconds();
-  Serial.print(String(currhour)+String(minutes)+String(secs));
-  //Serial.print("ASDRT"+ (String) timeClient.getFormattedTime());
-  // time_t now = time(nullptr);
-  // int our = hour(now);
+  currhour = timeClient.getHours();
+  minutes = timeClient.getMinutes();
+  secs = timeClient.getSeconds();
+  if (currhour<10) {
+    currhour1 += '0';
+    currhour1 += String(currhour);
+  }
+  else {
+    currhour1 += String(currhour);
+  }
+
+  if (minutes<10) {
+    minutes1 += '0';
+    minutes1 += String(minutes);
+  }
+  else {
+    minutes1 += String(minutes);
+  }
+
+  if (secs<10) {
+    secs1 += '0';
+    secs1 += String(secs);
+  }
+  else {
+    secs1 += String(secs);
+  }
+
+  Serial.print("T"+currhour1+minutes1+secs1);
+  //bot.sendMessage(chat_id, String(minutes1));
+  currhour1 = "";
+  minutes1 = "";
+  secs1 = "";
   if (millis() - bot_lasttime > BOT_MTBS)
   {
-    //timeClient.update();
+
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     while (numNewMessages)
     {
-      //Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
